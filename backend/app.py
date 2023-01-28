f64ffac7fd548ed94ccfe0d4b941deda6ad51e5c
Hari Dandapani <haridandapani@gmail.com>
2023-01-28 12:34:41 -0500

    adding basic flask request structure

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,37 @@
 from flask import Flask
+from flask import request
+import json
+import raketest
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/generate", methods = ['GET'])
+def generate_original_flashcards():
+    text_block = request.args.get("text")
+    flashcard_list = raketest.make_flashcards(text_block)
+    jsoner = jsonify_flashcard_list(flashcard_list)
+    response = app.response_class(
+        response=json.dumps(jsoner),
+        status=200,
+        mimetype='application/json'
+        )
+    return response
+
+def jsonify_flashcard_list(flashcard_list):
+    jsoned = list()
+    for flashcard in flashcard_list:
+        obj = {"original": flashcard.sentence,
+               "answer": flashcard.keyword,
+               "question": flashcard.flashcard_text}
+        jsoned.append(obj)
+    return jsoned
+
+def main():
+    app.run(threaded=True, port=5000)
+
+if __name__ == '__main__':
+    main()
